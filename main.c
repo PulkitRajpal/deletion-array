e26cf978650fd61aded1d9e4d93bd9c6a4e142ec
kgattani25 <43729365+kgattani25@users.noreply.github.com>
2018-11-10 18:44:23 +0530

    added new code

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[20],n,i,pos,val;
+    printf("enter the size of array");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("enter the elements of array");
+        scanf("%d",&a[i]);
+
+        }
+        printf("\n");
+        printf("enter the val of element");
+        scanf("%d",&val);
+    printf("enter the pos of element");
+    scanf("%d",&pos);
+    for(i=pos;i<n;i++)
+    {
+        a[i]=a[i+1];
+
+    }
+    n--;
+
+    for(i=0;i<n;i++)
+   {
+
+    printf(" %d ",a[i]);
+   }
+    return 0;
+}
